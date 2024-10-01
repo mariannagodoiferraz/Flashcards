6c39e7d077e7363b26cf0e56ea5725326af61db5
mariannagodoiferraz <145606092+mariannagodoiferraz@users.noreply.github.com>
2024-10-01 16:21:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior País do mundo em área territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Saúde',
+    'Qual a vitamina essencial para a saúde dos ossos?',
+    'Vitamina D'
+)
+
+criaCartao(
+    'Saúde',
+    'Quantos dentes permanentes possui um adulto saudável?',
+    '32 dentes'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem é conhecido como "rei do futebol"?',
+    'Pelé'
+)
